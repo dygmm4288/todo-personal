872861a644f0b19591223165e6a9b82a84757644
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-11-03 23:10:40 +0900

    redux 미들웨이 이용하여 로컬 스토리지 저장

--- src/store.js
+++ src/store.js
@@ -1,8 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
-import todoReducer from "./reducers/todoSlice";
+import todoReducer, { TODO_STORAGE_KEY } from "./reducers/todoSlice";
 
 export default configureStore({
   reducer: {
     todosReducer: todoReducer,
   },
+  middleware: [middlewareStorage],
 });
+
+function middlewareStorage(store) {
+  return function (next) {
+    return function (action) {
+      const result = next(action);
+      const { todos } = store.getState().todosReducer;
+      localStorage.setItem(TODO_STORAGE_KEY, JSON.stringify(todos));
+      return result;
+    };
+  };
+}
