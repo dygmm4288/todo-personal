e53f0dc1f63074614862d60ba3991e3c680a47dd
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-11-03 10:22:18 +0900

    프로젝트 초기화 및 기능 구현 완료

---
+++ src/containers/todo/TodoContainer.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import TodoForm from "../../components/todo/TodoForm";
+import TodoList from "../../components/todo/TodoList";
+import useStorage from "../../lib/useStorage";
+
+const TODO_STORAGE_KEY = "todos";
+
+export default function TodoContainer() {
+  const [getStorage, setStorage] = useStorage([], TODO_STORAGE_KEY, concat);
+  const [todos, setTodos] = useState(getStorage());
+  const [doneTodos, workingTodos] = todos.reduce(
+    (a, c) => {
+      a[c.isDone ? 0 : 1].push(c);
+      return a;
+    },
+    [[], []],
+  );
+
+  const reflectTodos = (nextTodos) => {
+    setTodos(nextTodos);
+    setStorage(nextTodos);
+  };
+  // 이벤트 핸들러
+  const handleEnrollTodo = (title, content) => (e) => {
+    e.preventDefault();
+    if (!title || !content) return;
+
+    const newTodo = {
+      title,
+      content,
+      isDone: false,
+      id: Date.now(),
+    };
+    reflectTodos(todos.concat(newTodo));
+  };
+
+  const handleRemoveTodo = (id) => () => {
+    reflectTodos(todos.filter((todo) => todo.id !== id));
+  };
+  const handleToggleTodo = (id) => () => {
+    reflectTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isDone: !todo.isDone,
+          };
+        }
+        return { ...todo };
+      }),
+    );
+  };
+
+  return (
+    <>
+      <TodoForm handleEnrollTodo={handleEnrollTodo} />
+      <div>
+        <h2>working...불꽃</h2>
+        <TodoList
+          todos={workingTodos}
+          handleRemoveTodo={handleRemoveTodo}
+          handleToggleTodo={handleToggleTodo}
+        />
+        <h2>Done...!폭죽</h2>
+        <TodoList
+          todos={doneTodos}
+          handleRemoveTodo={handleRemoveTodo}
+          handleToggleTodo={handleToggleTodo}
+        />
+      </div>
+    </>
+  );
+}
+
+const concat = (prev, next) => prev.concat(next);
---
+++ src/layout/Main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import TodoContainer from "../containers/todo/TodoContainer";
+
+export default function Main() {
+  return <TodoContainer />;
+}
