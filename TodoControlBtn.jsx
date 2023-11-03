2b50bbd847b3701b39edc68861e8ff36609fc434
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-11-03 09:26:33 +0900

    Initialize project using Create React App

--- TodoControlBtn.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function TodoControlBtn({ role, handleClickBtn, children }) {
-  return (
-    <button className={role} type='button' onClick={handleClickBtn}>
-      {children}
-    </button>
-  );
-}
