a7733e9a52b83d445c3e02d5b2e467c107e1a909
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-11-03 11:49:46 +0900

    기능 구현 중 오류 발생 수정 : storage에 연속으로 들어가는 이슈

--- src/lib/useStorage.jsx
+++ src/lib/useStorage.jsx
@@ -1,12 +1,11 @@
-export default function useStorage(defaultValue, defaultKey, nextFunc) {
+export default function useStorage(defaultValue, defaultKey) {
   // hook 느낌을 알기 위해서 의도적으로 작성한 코드
   const getStorage = (key) =>
     JSON.parse(localStorage.getItem(key || defaultKey)) || defaultValue;
   const setStorage = (value, key) => {
     if (!key) key = defaultKey;
-    const nextValue = nextFunc(getStorage(key), value);
-    localStorage.setItem(key, JSON.stringify(nextValue));
-    return { key: nextValue };
+    localStorage.setItem(key, JSON.stringify(value));
+    return { key: value };
   };
 
   return [getStorage, setStorage];
