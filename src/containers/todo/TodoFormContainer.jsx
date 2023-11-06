ce65f6d9b8c84dd8d4a661b507091f1d04b52fab
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-11-06 11:38:00 +0900

    favorite 추가

--- src/containers/todo/TodoFormContainer.jsx
+++ src/containers/todo/TodoFormContainer.jsx
@@ -1,13 +1,21 @@
 import React, { useRef, useState } from "react";
+import { useDispatch } from "react-redux";
 import TodoForm from "../../components/todo/TodoForm";
+import { addTodo } from "../../reducers/todoSlice";
 
-export default function TodoFormContainer({ handleEnrollTodo }) {
+export default function TodoFormContainer() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const titleRef = useRef(null);
+  const dispatch = useDispatch();
 
   const cleanInputValue = () =>
     [setTitle, setContent].forEach((func) => func(""));
+
+  const handleEnrollTodo = (title, content) => {
+    if (!title || !content) return;
+    dispatch(addTodo({ title, content }));
+  };
 
   const handleChange = (setter) => (e) => setter(e.target.value);
   const handleSubmit = (e) => {
