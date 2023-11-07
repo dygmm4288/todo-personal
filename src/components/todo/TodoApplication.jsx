79284b6428450c204e08df0c5b1fbc687e0a0173
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2023-11-07 22:57:51 +0900

    TodoApplication CSS -> styled 수정

--- src/components/todo/TodoApplication.jsx
+++ src/components/todo/TodoApplication.jsx
@@ -1,15 +1,35 @@
+import styled from "styled-components";
 import Header from "../../layout/todo/Header";
 import Main from "../../layout/todo/Main";
 import ApplicationWrapper from "../desktop/ApplicationWrapper";
-import "./TodoApplication.css";
 
 export default function TodoApplication(props) {
   return (
     <ApplicationWrapper {...props}>
-      <div id='todo-wrapper'>
+      <StyledTodoWrapper>
         <Header />
         <Main />
-      </div>
+      </StyledTodoWrapper>
     </ApplicationWrapper>
   );
 }
+
+const StyledTodoWrapper = styled.div`
+  min-width: 800px;
+  max-width: 1200px;
+  margin: 0 auto;
+  height: 100%;
+  padding: 1rem;
+  overflow-y: overlay;
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+  &::-webkit-scrollbar-thumb {
+    outline: none;
+    border-radius: 10px; /* 스크롤바 둥근 테두리 */
+    background-color: rgba(34, 34, 34, 0.6);
+  }
+  &::-webkit-scrollbar-track {
+    background: rgba(0, 0, 0, 0);
+  }
+`;
